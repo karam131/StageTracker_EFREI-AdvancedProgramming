e0f78c810d96d7249c6fcc959e938faafaa21a94
karam mansour <karammansour.km13@gmail.com>
2024-01-02 19:54:06 +0100

    shadcn set-up

---
+++ components/Navbar.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className='bg-zinc-100 py-2 border-b border-s-zinc-200 fixed w-full z-10 top-0'>
+      <div className='container'>
+        Login
+      </div>
+    </div>
+  )
+}
+
+export default Navbar
